8962d3384a263f3552f455d64c2e642faa56da43
Hamza Basrai <hamza.basrai@gmail.com>
2017-04-21 15:43:38 -0400

    Added CrimeLab

---
+++ app/src/main/java/com/hamzabasrai/criminalintent/CrimeLab.java
@@ -0,0 +1,48 @@
+package com.hamzabasrai.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Hamza Basrai on 4/21/2017.
+ */
+
+public class CrimeLab {
+
+    private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime # " + i);
+            crime.setSolved(i % 2 == 0); //Every other crime
+            mCrimes.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
