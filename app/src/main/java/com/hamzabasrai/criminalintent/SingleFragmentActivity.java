3765bbce49f25c82ae535ed819571123a0c13b62
Hamza Basrai <hamza.basrai@gmail.com>
2017-04-21 16:03:35 -0400

    Added SingleFragmentActivity and implementation

---
+++ app/src/main/java/com/hamzabasrai/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.hamzabasrai.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by Hamza Basrai on 4/21/2017.
+ */
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
