f45ed3f3335779f1747b43be9e9bf161308ed18b
Hamza Basrai <hamza.basrai@gmail.com>
2017-04-21 16:28:54 -0400

    Added CrimeHolder inner class

--- app/src/main/java/com/hamzabasrai/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/hamzabasrai/criminalintent/CrimeListFragment.java
@@ -1,13 +1,13 @@
 package com.hamzabasrai.criminalintent;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Hamza Basrai on 4/21/2017.
@@ -15,11 +15,20 @@ import android.view.ViewGroup;
 
 public class CrimeListFragment extends Fragment {
 
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTitleTextView;
+
+        public CrimeHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+    }
+
     private RecyclerView mCrimeRecyclerView;
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
         mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
