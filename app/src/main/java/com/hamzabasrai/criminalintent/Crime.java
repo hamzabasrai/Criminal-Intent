393d199e43c1d27c0ba5b015d2395d8bb93133df
Hamza Basrai <hamza.basrai@gmail.com>
2017-04-21 13:18:09 -0400

    Added Crime date and solved properties

--- app/src/main/java/com/hamzabasrai/criminalintent/Crime.java
+++ app/src/main/java/com/hamzabasrai/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.hamzabasrai.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,10 +10,14 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
 
     public Crime() {
         //Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -25,5 +30,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
