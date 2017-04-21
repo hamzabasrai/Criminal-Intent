ecf0e3c640dca9d04bf52a920a06dbe35ff5fef0
Hamza Basrai <hamza.basrai@gmail.com>
2017-04-21 16:25:19 -0400

    Added CrimeListFragment layout

--- app/src/main/java/com/hamzabasrai/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/hamzabasrai/criminalintent/CrimeListFragment.java
@@ -1,10 +1,28 @@
 package com.hamzabasrai.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Hamza Basrai on 4/21/2017.
  */
 
 public class CrimeListFragment extends Fragment {
+
+    private RecyclerView mCrimeRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }
