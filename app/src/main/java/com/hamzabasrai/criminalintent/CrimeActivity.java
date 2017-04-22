78c14ecee9e9bae3ec29628c202c0d6298e0e540
Hamza Basrai <hamza.basrai@gmail.com>
2017-04-22 12:46:42 -0400

    Refactored CrimeFragment and CrimeActivity with fragment arguments

--- app/src/main/java/com/hamzabasrai/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/hamzabasrai/criminalintent/CrimeActivity.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
-    public static final String EXTRA_CRIME_ID = "com.hamzabasrai.criminalintent.crime_id";
+    private static final String EXTRA_CRIME_ID = "com.hamzabasrai.criminalintent.crime_id";
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimeActivity.class);
@@ -18,6 +18,7 @@ public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
