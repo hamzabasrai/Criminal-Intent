e090e50680c3a3e912127af1e90e33951469ab2f
Hamza Basrai <hamza.basrai@gmail.com>
2017-04-24 09:58:28 -0400

    DatePicker now shows current date

--- app/src/main/java/com/hamzabasrai/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/hamzabasrai/criminalintent/CrimeFragment.java
@@ -76,7 +76,7 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 FragmentManager manager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 dialog.show(manager, DIALOG_DATE);
             }
         });
--- app/src/main/java/com/hamzabasrai/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/hamzabasrai/criminalintent/DatePickerFragment.java
@@ -7,6 +7,10 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Hamza Basrai on 4/24/2017.
@@ -14,10 +18,33 @@ import android.view.View;
 
 public class DatePickerFragment extends DialogFragment {
 
+    private static final String ARG_DATE = "date";
+
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+        mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_date_picker);
+        mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
